261e093201bd8c47584758324fdeb428083e25cf
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2021-02-23 22:35:59 -0300

    initial commit docker structure

---
+++ helloworld/views.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+logger = logging.getLogger(__name__) 
+
+def hello_world(request):
+    version_var = "v3"
+    logger.debug(f'A debug message {version_var}')
+    logger.info(f'An info message {version_var}')
+    logger.warning(f'A warning message {version_var}')
+    logger.error(f'An error message {version_var}')
+    logger.critical(f'A critical message {version_var}')
+
+    return HttpResponse("You're looking at question")
